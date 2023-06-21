1ef6d3318f53902e3a5491106cdbecba92b81b55
Kooreis <kooreis.official@gmail.com>
2023-06-20 23:06:40 -0500

    The first part of the script includes the necessary namespaces, defines the main class and the Main method. It also prompts the user to input a word and a list of words, which are then stored in variables.

--- Anagram_Finder.cs
+++ Anagram_Finder.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,36 +11,4 @@ class Program
         Console.WriteLine("Enter a list of words (separated by comma):");
         string words = Console.ReadLine();
         List<string> wordList = words.Split(',').ToList();
-
-        List<string> anagrams = FindAnagrams(word, wordList);
-
-        Console.WriteLine("Anagrams found:");
-        foreach (string anagram in anagrams)
-        {
-            Console.WriteLine(anagram);
-        }
     }
-
-    static List<string> FindAnagrams(string word, List<string> candidates)
-    {
-        List<string> anagrams = new List<string>();
-        foreach (string candidate in candidates)
-        {
-            if (IsAnagram(word, candidate))
-            {
-                anagrams.Add(candidate);
-            }
-        }
-        return anagrams;
-    }
-
-    static bool IsAnagram(string word, string candidate)
-    {
-        char[] wordArray = word.ToLower().ToCharArray();
-        char[] candidateArray = candidate.ToLower().ToCharArray();
-        Array.Sort(wordArray);
-        Array.Sort(candidateArray);
-        return new string(wordArray) == new string(candidateArray);
-    }
-}
-```
